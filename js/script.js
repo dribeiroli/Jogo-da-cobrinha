aa4f88b77f277cee901d6b8dd3df404c9f97129c
Adriana Ribeiro <adrisenna@gmail.com>
2020-05-18 20:09:27 -0300

    adição cobra

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let canvas= document.getElementById('snake');
 let context= canvas.getContext('2d');
 let box= 32;
+let snake =[];
+snake[0] ={
+    x: 8*box,
+    y: 8*box
+}
 
 function criarBG() {
     context.fillStyle ="lightgreen";
@@ -8,3 +13,12 @@ function criarBG() {
 }
 
 criarBG();
+
+function criarCobrinha(){
+    for(i=0; i<snake.length; i++){
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+criarCobrinha();
